1f8b62235b4500127ac169b0ebd4e0a64427107f
kutayeroglu <49415251+kutayeroglu@users.noreply.github.com>
2026-03-28 18:49:23 +0000

    Deploying to gh-pages from @ kutayeroglu/kutayeroglu.github.io@edb8ed61319d0d998e83a59be72e65431b0c2585 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -433,16 +433,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-i-started-pursuing-my-m-sc-in-computer-engineering-at-bogazici-university",
+          title: 'I started pursuing my M.Sc. in Computer Engineering at Bogazici University.',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-i-started-working-as-a-graduate-research-and-teaching-assistant-at-the-computer-engineering-department-at-bogazici-university",
+          title: 'I started working as a Graduate Research and Teaching Assistant at the Computer...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
